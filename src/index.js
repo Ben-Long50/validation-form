8ebeb332df1ab7bd6fab6d68486bd0d194d49ea2
Ben-Long50 <benjlong50@gmail.com>
2024-04-02 05:45:37 -0700

    added country-codes-list and postal-codes-js libraries for form options

--- src/index.js
+++ src/index.js
@@ -1,2 +1,20 @@
 import './styles/main.css';
 import './styles/reset-css.css';
+import countryCodes from 'country-codes-list';
+import postalCodes from 'postal-codes-js';
+
+const countryList = countryCodes.customList('countryCode', '{countryNameEn}');
+const countryNames = Object.values(countryList);
+
+console.log(countryNames);
+const countryInput = document.querySelector('#country');
+
+function populateCountryOptions() {
+  countryNames.forEach((element) => {
+    const optionElement = document.createElement('option');
+    optionElement.textContent = element;
+    countryInput.appendChild(optionElement);
+  });
+}
+
+populateCountryOptions();
